4eff1cfd6c658c4bc240f1c87f70158a36e1761f
Saúl González <saulegonzalez58@gmail.com>
2024-05-01 03:44:26 +0000

    Update database reference and add new functionality in index.html and script.js

--- script.js
+++ script.js
@@ -3,29 +3,43 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set } from "firebase/database"; // Import the necessary functions for database operations
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCG1Gw4TMZFiPUCjw2Fdifpz9PYt65iNGQ",
-  authDomain: "abogados-d3805.firebaseapp.com",
-  projectId: "abogados-d3805",
-  storageBucket: "abogados-d3805.appspot.com",
-  messagingSenderId: "761918186682",
-  appId: "1:761918186682:web:53503914902036a07cce83",
-  measurementId: "G-P1D4CW0P4R"
+var firebaseConfig = {
+    apiKey: "AIzaSyCG1Gw4TMZFiPUCjw2Fdifpz9PYt65iNGQ",
+    authDomain: "abogados-d3805.firebaseapp.com",
+    projectId: "abogados-d3805",
+    storageBucket: "abogados-d3805.appspot.com",
+    messagingSenderId: "761918186682",
+    appId: "1:761918186682:web:53503914902036a07cce83",
+    measurementId: "G-P1D4CW0P4R"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-// Get a reference to the database service
-const db = getDatabase(app);
+function writeToDatabase() {
+  document.getElementById('statusText').textContent = 'Failed to write data';
 
-// Create a reference to the location you want to write to
-const dbRef = ref(db, 'Clientes/0');
+  // Get a reference to the database service
+  var db = firebase.database();
 
-// Write the data
-set(dbRef, {name: 'value'}).then(() => {
-  console.log('Data written successfully');
-}).catch((error) => {
-  console.error('Failed to write data:', error);
-});
+  // Create a reference to the location you want to write to
+  var dbRef = db.ref('Clientes/0');
+
+  // Write the data
+  dbRef.set({name: 'value'}).then(() => {
+    console.log('Data written successfully');
+
+    // Change the text of an HTML element
+    document.getElementById('myText').textContent = 'Data written successfully';
+  }).catch((error) => {
+    console.error('Failed to write data:', error);
+
+    // Change the text of an HTML element
+
+  });
+}
+
+function changeColor() {
+  document.getElementById("my-div").style.backgroundColor = "#00FF00";
+}
